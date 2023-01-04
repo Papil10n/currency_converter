159500a5fab9d9c1acd11753098350493d26684b
Papil10n <nikEpt.co@gmail.com>
2023-01-05 00:12:00 +0200

    int project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="converter dark">
+            <div className="wrapper dark:bg-[#353535] w-screen h-screen">
+                <div className="container max-w-5xl my-0 mx-auto">
+                    {/*<Header/>*/}
+                    {/*<Main/>*/}
+                    {/*<Footer/>*/}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
